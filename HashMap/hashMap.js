d06af03da946d242742d228ac3e18e1b5b45212a
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2024-12-23 11:25:28 +0000

    Implement the hash function for the hashmap

---
+++ HashMap/hashMap.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {String} key The key to hash
+ * @returns {Number} Returns the hashcode
+ */
+function hash(key){
+    let hashcode = 0;
+    const primeNumber = 31;
+
+    for(let i = 0; i < key.length; i++){
+        hashcode = primeNumber * hashcode + key.charCodeAt(i);
+    }
+
+    return hashcode;
+}
