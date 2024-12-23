e8d71ec3f8a5130fb9a6c93d54327fe8ba0abf7f
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2024-12-23 10:31:27 +0000

    Implement the at and pop method of the linkedList

--- linkedList.js
+++ linkedList.js
@@ -38,6 +38,43 @@ function createLinkedList(){
         if(node === null) return count;
         return size(count + 1, node.nextNode);
     }
+
+    
+    /**
+     * 
+     * @param {Number} index The index of the node we should find
+     * @param {Number} count The number to increment when we are iterating throught linked list
+     * @param {Object} node The head of the linked list
+     * @returns {(Number|Null)} Returns the indexed node or null if it doesn't exist
+     */
+    function at (index, count = 0, node = head){
+        if(!node) return null;
+        if(count === index) return node;
+        return at(index, count + 1, node.nextNode);
+    }
+
+    /**
+     * 
+     * @param {Object} node The head of the linkedlist
+     * @param {Object} previousNode To keep a trace of the previous node when traversing
+     * @returns Returns nothings, just remove the last node of the linked list
+     */
+    function pop(node = head, previousNode){
+        if(node.nextNode ===  null){
+            tail = previousNode;
+            if(previousNode) {
+                previousNode.nextNode = null;
+            } else {
+                head = tail = null;
+            }
+            return;
+        } 
+
+        previousNode = node;
+        pop(node.nextNode, previousNode);
+    }
+
+    
 }
 
 function createNode(value){
